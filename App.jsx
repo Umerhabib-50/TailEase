9fa2c2e2ac9f6eb0c8aae3a2a39dd6b868bb2b9a
Rehan khan <rehankhanabdali@gmail.com>
2023-08-21 18:24:03 +0500

    code merged

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const App = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: '#800080'}}>
+      <Text>App</Text>
+    </View>
+  );
+};
